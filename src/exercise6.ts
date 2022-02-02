c5b47393b8c728e818dbc7d0471d14241999d14b
Lawrence Yoon <lyoon@truechoice.io>
2022-02-02 11:12:36 -0800

    refactor: delete a lot of functions, going through one by one

---
+++ src/exercise6.ts
@@ -0,0 +1,40 @@
+interface User {
+  type: 'user';
+  name: string;
+  age: number;
+  occupation: string;
+}
+
+interface Admin {
+  type: 'admin';
+  name: string;
+  age: number;
+  role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+  {
+    type: 'user',
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep'
+  },
+  { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+  { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+  { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+  { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+  { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+];
+
+// takes in an object in format of User | Admin
+export function logPerson(person: Person): void {
+  console.log(
+    ` - ${person.name}, ${person.age}, ${
+      person.type === 'admin' ? person.role : person.occupation
+    }`
+  );
+}
+
+persons.forEach(logPerson);
